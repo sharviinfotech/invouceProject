2941587cbc97d5f72f2010dad032b52b4da6fdaf
sparkle243 <sunilakula243@gmail.com>
2025-01-28 15:27:08 +0530

    menu invoice channges

--- src/app/layouts/sidebar/menu.ts
+++ src/app/layouts/sidebar/menu.ts
@@ -13,17 +13,18 @@ export const MENU: MenuItem[] = [
                 parentId: 2
             },
             {
-                id: 3,
-                label: 'Invoice',
-                link: '/Invoice',
-                parentId: 1
-            },
-            {
                 id: 4,
                 label: 'Invoice Layout',
                 link: '/InvoiceLayout',
                 parentId: 1
             },
+            {
+                id: 3,
+                label: 'Invoice',
+                link: '/Invoice',
+                parentId: 1
+            },
+           
         ]
     },
     // {
