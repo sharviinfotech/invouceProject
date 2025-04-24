c0f08c0a3488ba2e0c856a34b22345baf5072c27
Sunil Kumar <sunilkumar@sharviinfotech.com>
2025-04-24 12:52:21 +0530

    changes in env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,5 +12,6 @@ export const environment = {
     measurementId: ''
   },
   // baseUrl:'http://localhost:3000/api/',
-  baseUrl:'http://125.22.172.147:3000/api/',
+  // baseUrl:'http://125.22.172.147:3000/api/',
+  baseUrl:'http://192.168.1.18:3000/api/',
 };
